01c2815fd60c96ddc4db489a978fd88a1cd0e48b
Evan Klei <elk7926@rit.edu>
2016-11-09 15:08:02 -0500

    Modified arguements, added hard limit to the circle size

--- corobot_coop/src/viewImage.py
+++ corobot_coop/src/viewImage.py
@@ -3,6 +3,7 @@
 import rospy
 import cv2
 from sensor_msgs.msg import Image, LaserScan
+from corobot_common.msg import Pose
 from cv_bridge import CvBridge, CvBridgeError
 from nav_msgs.msg import Odometry
 import numpy as np
@@ -17,7 +18,7 @@ class ImageViewer():
     def __init__( self ):
 	self.bridge = CvBridge()
 	self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.image_call)
-	self.pos_sub = rospy.Subscriber("/odom", Odometry, self.odom_call)
+	self.pos_sub = rospy.Subscriber("/pose", Pose, self.odom_call)
 	self.scan = rospy.Subscriber("/scan", LaserScan, self.scan_call)
 	self.images = []
 	self.pos = []
@@ -30,7 +31,7 @@ class ImageViewer():
 	self.images.append(data)
 
     def odom_call(self, data):
-	self.pos.append( data.pose.pose )
+	self.pos.append( data )
 
     def scan_call(self, data):
 	self.scan.append( data )
@@ -50,11 +51,12 @@ class ImageViewer():
 	    except CvBridgeError as e:
 		print e
 	    gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-	    edges = cv2.Canny(gray_image, 100, 200)
+	    edges = cv2.Canny(gray_image, 300, 600)
 	    circles = cv2.HoughCircles( edges, cv2.HOUGH_GRADIENT, 1, 20, 100, 50, 5, 10 )
 	    output = cv_image.copy()
 	    if circles is not None:
-		circles = np.round(circles[0, :]).astype("int")[:7]
+		circles = np.round(circles[0, :]).astype("int")
+		circles = filter((lambda x: x[2] < 30), circles)[:min(10, len(circles))]
 		for i in range(len(circles)):
 		     robot = []
                      robot.append(circles[i])
@@ -76,17 +78,15 @@ class ImageViewer():
 			    break
 			phi_obs = self.seq.index(colorstring[:2]) * np.pi / 8.0
 			print phi_obs, colorstring[:2]
-			d = scan.ranges[max(len(scan.ranges) - robot[0][0] - 10, 0): min(len(scan.ranges) - robot[0][0]+10, len(scan.ranges))]
+			d = scan.ranges[max(len(scan.ranges) - robot[0][0] - 20, 0): min(len(scan.ranges) - robot[0][0]+20, len(scan.ranges))]
 			d = np.nanmean(d)
 			if np.isnan(d):
 			    print( 'No Object Observed in this direction' )
 			    break
 			camang = scan.angle_min + (len(scan.ranges) - robot[0][0]) * scan.angle_increment
-			px = pos.position.x
-			py = pos.position.y
-			qw = pos.orientation.w
-			qz = pos.orientation.z
-			ptheta = np.arctan2(2 * qw * qz, 1 - 2 * qz * qz)
+			px = pos.x
+			py = pos.y
+			ptheta = pos.theta
 			print("ME:")
 			print(px, py, ptheta)
 			
@@ -95,55 +95,27 @@ class ImageViewer():
 			otheta = (np.pi - phi_obs) + (ptheta + camang)
 			print ("THEM: ")
 			print (ox, oy, otheta)
+			print ( scan.header.stamp )
 			break	
 	    cv2.imshow( "Test", output)
 	    cv2.waitKey(3)
 
     def getColor(self, x, y, radius, image ):
 	img_lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-	colors = np.zeros((20, 1, 3), dtype = "uint8")
-	colors[0] = [92, 171, 154] #Red
-	colors[1] = [156, 144, 141] #Red
-	colors[2] = [109, 165, 149] #Red
-	colors[3] = [137, 152, 142] #Red
-	colors[4] = [66, 119, 135] #Green
-	colors[5] = [78, 118, 136] #Green
-	colors[6] = [72, 130, 111] #Blue
-	colors[7] = [98, 130, 110] #Blue
-	colors[8] = [37, 129, 129] #Black
-	colors[9] = [173, 130, 135] #White
-	colors[10] =  [108, 129, 131] #White
-	colors[11] = [150, 129, 140] #White?
-	colors[12] = [218, 128, 131] #White
-	colors[13] = [138, 128, 134] #White
-	colors[14] = [91, 126, 133] #White?
-	colors[15] = [199, 129, 133] #White
-	colors[16] = [65, 131, 130] #White? Could be black
-	colors[17] = [159, 128, 133]
-	colors[18] = [188, 130, 132]
-	colors[19] = [123, 128, 133]
 
-	minDist = (np.inf, "?")
-	lab = colors
 	mask = np.zeros(image.shape[:2], dtype="uint8")
 	cv2.circle( mask, (x, y), 5, 255, -1)
 	col = cv2.mean(img_lab, mask = mask)[:3]
-	self.colorRecord.write( str(col[0]) + " " + str(col[1]) + " "+ str(col[2]) + "\n" );
-	for i in range(len(lab)):
-            dis = dist(col, lab[i][0].astype('double'))
-	    if minDist[0] > dis:
-		if i >= 0 and i<= 3:
-		    minDist = (dis, "R")
-		elif i == 4 or i == 5:
-		    minDist = (dis, "G")
-		elif i == 6 or i == 7:
-		    minDist = (dis, "B")
-		elif i == 8:
-		    minDist = (dis, "K")
-		else:
-		    minDist = (dis, "W")
-
-	return minDist[1]
+        if col[1] >= 140:
+	    return 'R'
+	elif col[1] <= 125:
+	    return 'G'
+	elif col[2] <= 120:
+	    return 'B'
+        elif col[0] <= 50:
+	    return 'K'
+	else:
+	    return 'W'
 
 if __name__ == "__main__":
     iv = ImageViewer()
