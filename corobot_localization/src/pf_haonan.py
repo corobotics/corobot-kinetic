0d08e059c0ebbf41d60bdcee329ffff4636d956e
HaonanYin <yinhao198808@gmail.com>
2017-11-27 16:57:25 -0500

    Particle Filtering test

--- corobot_localization/src/pf_haonan.py
+++ corobot_localization/src/pf_haonan.py
@@ -26,6 +26,9 @@ def pf_initialize(qrcode_pose):
     orientation = qrcode_pose.theta
     mean = 0
     covariance = tuple(qrcode_pose.cov)
+    print(len(covariance))
+    for each in covariance:
+        print(each)
     init_probability = 1 / num_particles
 
     particle_count = 0
