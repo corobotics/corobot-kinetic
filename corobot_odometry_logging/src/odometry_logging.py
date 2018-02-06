037f4729727a88d8f53b6df38bf6bdd80f176b74
ruturaj001 <ruturaj1989@gmail.com>
2018-02-06 13:51:38 -0500

    cmd vel and odom logging

--- corobot_odometry_logging/src/odometry_logging.py
+++ corobot_odometry_logging/src/odometry_logging.py
@@ -2,30 +2,50 @@
 
 import roslib; roslib.load_manifest("corobot_odometry_logging")
 import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
 
 FEATURES = {
-    'odom': None;
+    'odom': None,
+    'cmd_vel': None
 }
 
 def odom_callback(odom):
     global FEATURES
     FEATURES['odom'] = odom
 
+def cmd_vel_callback(vel):
+    global FEATURES
+    print(vel)
+    FEATURES['cmd_vel'] = vel
+
 def get_feature_vector():
-    if not FEATURES['odom']:
+    if not FEATURES['odom'] or not FEATURES['cmd_vel']:
         return None
     return [
         FEATURES['odom'].pose.pose.position.x,
         FEATURES['odom'].pose.pose.position.y,
         FEATURES['odom'].pose.pose.orientation.z,
-        FEATURES['odom'].pose.pose.orientation.w
+        FEATURES['odom'].pose.pose.orientation.w,
+        FEATURES['cmd_vel'].linear.x,
+        FEATURES['cmd_vel'].linear.y,
+        FEATURES['cmd_vel'].angular.z
     ]
 
 def log_status():
-    rospy.loginfo(
+    rospy.loginfo(get_feature_vector())
 
 def main():
     rospy.init_node("odometry_logging")
     rospy.Subscriber("odom", Odometry, odom_callback)
-    rospy.Timer(rospy.Duration(0.01), log_status)
+    rospy.Subscriber("/mobile_base/commands/velocity", Twist, cmd_vel_callback)
+
+    while not rospy.is_shutdown():
+        log_status()
     rospy.spin()
+
+if __name__=='__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
